4a0bdad75f1b19abfd233ee5aac6466842a5d410
levicollier <52548321+levicollier@users.noreply.github.com>
2025-10-18 16:47:14 -0700

    Delete src/pages/index.js

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function IndexPage() {
-  return (
-    <main style={{padding:'2rem'}}>
-      <h1>Reset Build</h1>
-      <p>If you see this, the rebuild worked.</p>
-    </main>
-  );
-}
