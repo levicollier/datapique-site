70f7249cfa6f378f885812c3bc3574ee4fbc8d1e
levicollier <52548321+levicollier@users.noreply.github.com>
2025-10-18 16:15:55 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,8 @@
-
+export default function IndexPage() {
+  return (
+    <main style={{padding:'2rem'}}>
+      <h1>Reset Build</h1>
+      <p>If you see this, the rebuild worked.</p>
+    </main>
+  );
+}
